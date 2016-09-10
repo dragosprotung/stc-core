6630e0d9eca451f5654248ebf33c52817cc763d6
Dragos Protung <dragos@protung.ro>
2016-09-10 18:14:01 +0200

    Added InvalidCredentialsException

---
+++ src/Tracker/Exception/InvalidCredentialsException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SportTrackerConnector\Core\Tracker\Exception;
+
+/**
+ * Exception to throw when authentication fails in a tracker.
+ */
+class InvalidCredentialsException extends \RuntimeException
+{
+}
